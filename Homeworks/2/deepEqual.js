6612a79a3952b6a784bd11ed973198c23aba5618
Georgy Bazhenov <tassadar.xxx@gmail.com>
2024-04-13 00:10:41 +0500

    Изменена функция

--- Homeworks/2/deepEqual.js
+++ Homeworks/2/deepEqual.js
@@ -14,59 +14,43 @@ const obj3 = {
     },
 };
 function deepEqual(actual, expected) {
-    function _deepEqual(actual, expected, message, path) {
-        function getResult(r, m, p = "") {
-            return { result: r, path: path + p, message: message + m };
+    function getResult(r, m, p = "") {
+        return { result: r, path: p, message: m };
+    }
+    if (actual === expected)
+        return getResult(true, "Значения/ссылки равны");
+    if (typeof (expected) == "object") {
+        if (Array.isArray(actual) && Array.isArray(expected)) {
+            if (actual.length != expected.length) {
+                return getResult(false, "Длины массивов не равны");
+            }
+            for (let i = 0; i < expected.length; i++) {
+                let deResult = deepEqual(actual[i], expected[i]);
+                if (!deResult.result) {
+                    return getResult(false, deResult.message, `[${i}]` + deResult.path);
+                }
+            }
+            return getResult(true, "Массивы равны");
         }
-        // Быстрое сравнение по ссылке или значению
-        let result = actual === expected;
-        if (result)
-            return getResult(true, "Значения/ссылки равны");
-        let actualTypeName = typeof (actual);
-        let expectedTypeName = typeof (expected);
-        // Быстрое сравнение по типу
-        if (actualTypeName != expectedTypeName)
-            return getResult(false, "Типы объектов не равны");
-        switch (actualTypeName) {
-            case "boolean":
-            case "number":
-            case "bigint":
-            case "string":
-                return getResult(result, "Значения не равны");
-            case "null":
-            case "undefined":
-                return getResult(result, "Одно из значений не определено");
-            case "function":
-                result = actual.toString() == expected.toString();
-                return getResult(result, result ? "Функции равны" : "Функции не равны");
-            case "object":
-                if (Array.isArray(actual) && Array.isArray(expected)) {
-                    if (actual.length != expected.length)
-                        return getResult(false, "Длины массивов не равны");
-                    for (let i = 0; i < actual.length; i++) {
-                        let deResult = _deepEqual(actual[i], expected[i], message, path);
-                        if (!deResult.result)
-                            return getResult(false, deResult.message, deResult.path + `[${i}]`);
+        else {
+            let propertyNames = Object.keys(expected);
+            for (let propertyName of propertyNames) {
+                if (actual.hasOwnProperty(propertyName)) {
+                    let deResult = deepEqual(actual[propertyName], expected[propertyName]);
+                    if (!deResult.result) {
+                        return getResult(false, deResult.message, `.${propertyName}` + deResult.path);
                     }
-                    return getResult(true, "Массивы равны");
                 }
                 else {
-                    let actualPropertyNames = Object.keys(actual);
-                    let expectedPropertyNames = Object.keys(actual);
-                    if (actualPropertyNames.length != expectedPropertyNames.length)
-                        return getResult(false, "Количество свойств не совпадает");
-                    for (let propertyName of expectedPropertyNames) {
-                        let deResult = _deepEqual(actual[propertyName], expected[propertyName], message, path);
-                        if (!deResult.result)
-                            return getResult(false, deResult.message, deResult.path + `.${propertyName}`);
-                    }
-                    return getResult(true, "Объекты равны");
+                    return getResult(false, "Нет свойства", `.${propertyName}`);
                 }
-            default:
-                throw new Error(`Тип \"${actualTypeName}\" не поддерживается для сравнения`);
+            }
+            return getResult(true, "Объекты равны");
         }
     }
-    return _deepEqual(actual, expected, "", "");
+    else {
+        return getResult(false, "Значения/ссылки не равны");
+    }
 }
 console.log(deepEqual(obj1, obj1));
 console.log(deepEqual(obj1, obj2));
